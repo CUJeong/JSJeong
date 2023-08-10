2b33ce14ad6350fda36324e90cafbca2b0078178
CUJeong <akow283@gmail.com>
2023-08-10 17:42:29 +0900

    js

---
+++ resources/js/myJs.js
@@ -0,0 +1,10 @@
+// script 태그 내에 작성하던 javaScript 코드 작성
+function f_rgbColor(){
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+
+    return "rgb("+red+", "+blue+", "+green+")";
+}
+
+const v_phone = "01073987332";
